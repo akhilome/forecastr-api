f570b8be907e181ee6e1bfa6322c0265cdaee6ec
Kizito Akhilome <hovkard@gmail.com>
2019-09-03 04:13:33 +0100

    config cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import env from './config/environment';
 import router from './routes/index.route';
 
@@ -6,6 +7,27 @@ const { port } = env;
 
 const app = express();
 
+const whitelist = ['https://forecastr-api.herokuapp.com'];
+const corsOptions = {
+  origin: (origin, cb) => {
+    if (
+      whitelist.indexOf(origin) !== -1 ||
+      origin.startsWith('http://localhost')
+    ) {
+      cb(null, true);
+    } else {
+      cb(new Error('CORS: Not allowed'));
+    }
+  }
+};
+
+app.use(cors(corsOptions), (err, req, res, next) => {
+  if (err) {
+    res.status(400).json({ success: false, message: 'blocked by cors' });
+  } else {
+    next();
+  }
+});
 app.use('/api/v1', router);
 
 app.get('/', (req, res) => {
